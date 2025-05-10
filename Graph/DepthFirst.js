575a91bcfd7a9146ca0143032793111f357da712
Cristian <cristiancandidatu@hotmail.com>
2025-05-10 16:53:50 +0300

    pascal's triangle

--- Graph/DepthFirst.js
+++ Graph/DepthFirst.js
@@ -1,21 +1,21 @@
-// const depthFirstPrint = (graph, source) => {
-//   const stack = [source];
-//   while (stack.length > 0) {
-//     const current = stack.pop();
-//     console.log(current);
+const depthFirstPrint = (graph, source) => {
+  const stack = [source];
+  while (stack.length > 0) {
+    const current = stack.pop();
+    console.log(current);
 
-//     for (let neighbor of graph[current]) {
-//       stack.push(neighbor);
-//     }
-//   }
-// };
-// const graph = {
-//   a: ["b", "c"],
-//   b: ["d"],
-//   c: ["e"],
-//   d: ["f"],
-//   e: [],
-//   f: [],
-// };
+    for (let neighbor of graph[current]) {
+      stack.push(neighbor);
+    }
+  }
+};
+const graph = {
+  a: ["b", "c"],
+  b: ["d"],
+  c: ["e"],
+  d: ["f"],
+  e: [],
+  f: [],
+};
 
-// depthFirstPrint(graph, "a");
+depthFirstPrint(graph, "a");
