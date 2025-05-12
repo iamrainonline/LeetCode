f5c3db3620eebd57ac80575a6331b6b73a7aafcc
Cristian <cristiancandidatu@hotmail.com>
2025-05-12 17:29:51 +0300

    cartesian product

---
+++ Algorithms/11_CartesianProduct.js
@@ -0,0 +1,15 @@
+const num1 = [1, 2];
+const num2 = [3, 4];
+
+// Add  num1 * num2
+const cartesianProduct = (num1, num2) => {
+  let result = [];
+  for (let i = 0; i < num1.length; i++) {
+    for (let j = 0; j < num2.length; j++) {
+      result.push([num1[i], num2[j]]);
+    }
+  }
+  return result;
+};
+
+console.log(cartesianProduct(num1, num2));
