83b606a72245e56789b8423213e49b9ba46383e1
Cristian <cristiancandidatu@hotmail.com>
2025-05-17 17:21:17 +0300

    subarray

---
+++ Algorithms/12_MaxSubarray.js
@@ -0,0 +1,13 @@
+var maxSubArray = function (nums) {
+  let currMax = nums[0];
+  let maxima = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    currMax = Math.max(nums[i], currMax + nums[i]);
+    maxima = Math.max(maxima, currMax);
+  }
+
+  return maxima;
+};
+
+console.log(maxSubArray([-2, 1 - 3, 4, -1, 2, 1, -5, 4]));
