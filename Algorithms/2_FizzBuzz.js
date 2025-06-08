a3d3527dedeaf6a1abf68f92228cea96e227db8c
Cristian <cristiancandidatu@hotmail.com>
2025-06-08 14:32:53 +0300

    practice

--- Algorithms/2_FizzBuzz.js
+++ Algorithms/2_FizzBuzz.js
@@ -1,15 +1,15 @@
 var fizzBuzz = function (n) {
-   let answer = [];
-   for (let i = 1; i <= n; i++) {
-      if (i % 3 == 0 && i % 5 == 0) {
-         answer[i] = "FizzBuzz";
-      } else if (i % 3 == 0) {
-         answer[i] = "Fizz";
-      } else if (i % 5 == 0) {
-         answer[i] = "Buzz";
-      } else answer[i] = "" + i;
-   }
-   return answer;
+  let answer = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      answer[i] = "FizzBuzz";
+    } else if (i % 3 == 0) {
+      answer[i] = "Fizz";
+    } else if (i % 5 == 0) {
+      answer[i] = "Buzz";
+    } else answer[i] = "" + i;
+  }
+  return answer;
 };
 
-// console.log(fizzBuzz(6));
+console.log(fizzBuzz(6));
--- challenge.js
+++
@@ -1,6 +0,0 @@
-const map = new Map();
-map.set("a", 1);
-
-for (const [key, value] of map) {
-  console.log(key, value);
-}
