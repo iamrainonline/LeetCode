23ee49b23cc09358cc51160d602b86ac51bc7de3
Cristian C. <cristiancandidatu@hotmail.com>
2025-08-24 21:11:51 +0300

    sets

--- challenge.js
+++ challenge.js
@@ -45,20 +45,6 @@ const fastFunction = memoize(ridiculousSlowFunc);
 // console.log(fastFunction(3000000000));
 // console.timeEnd();
 
-var isValid = function (s) {
-  const hashmap = { "(": ")", "{": "}", "[": "]" };
-  const stack = [];
-  for (char of s) {
-    if (char in hashmap) {
-      stack.push(hashmap[char]);
-    } else if (stack[stack.length - 1] === char) {
-      stack.pop();
-    } else {
-      return false;
-    }
-  }
-};
-
 // console.log(isValid("{[]}"));
 const input = document.querySelector("#mybtn");
 
@@ -73,23 +59,36 @@ const onInput = (event) => {
   }, 1000);
 };
 
-input.addEventListener("onkeypress", onInput);
+// input.addEventListener("onkeypress", onInput);
 
 // find duplicates and return []
 
-let nums = [7, 1, 2, 3, 4, 3, 7, 2];
-// 2,3
-
-const findDuplicates = (arr) => {
-  let duplicates = [];
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (newArr.includes(arr[i])) {
-      duplicates.push(arr[i]);
-    }
-    newArr.push(arr[i]);
+const cTree = (num) => {
+  for (let i = 1; i <= num; i++) {
+    const spaces = " ".repeat(num - i);
+    const stars = "*".repeat(2 * i - 1);
+    console.log(spaces + stars);
   }
-  return duplicates;
+
+  // Add a little trunk
+  const trunkSpaces = " ".repeat(num - 1);
+  console.log(trunkSpaces + "|");
 };
 
-console.log(findDuplicates(nums));
+// cTree(3);
+
+// let var in loop
+
+for (var i = 0; i < 3; i++) {
+  (function (x) {
+    setTimeout(() => {
+      // console.log(x);
+    }, 1000);
+  })(i);
+}
+
+for (var i = 0; i < 3; i++) {
+  // setTimeout(console.log.bind(null, i), 1000);
+}
+
+// set
